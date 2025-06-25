ba2876e1e91dee55980571cf1e58d5c0ed469b6b
perelyaev <perelyaev@gmail.com>
2025-06-25 17:38:06 +0800

    edit metod signin in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
+import * as bcrypt from 'bcryptjs';
 import { Injectable } from '@nestjs/common';
 import { User } from './user.type';
 
 @Injectable()
 export class AuthService {
   signin(user: User) {
-    return user;
+    return bcrypt.hashSync(Math.floor(100000 + Math.random() * 900000).toString(), 10);
   }
 }
