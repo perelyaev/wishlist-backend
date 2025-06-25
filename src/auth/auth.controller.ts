4d5229ff8c71fe739304cd98cc60ef41229231fb
perelyaev <perelyaev@gmail.com>
2025-06-25 09:42:48 +0800

    add metod signin in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserDto } from './dto/user.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  signin(@Body() { email }: UserDto) {
+    return this.authService.signin({ email });
+  }
 }
