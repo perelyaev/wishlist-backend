02a12ff6c6350b5fc6301caf0df296e5a96d7349
perelyaev <perelyaev@gmail.com>
2025-07-30 22:28:40 +0800

    edit MailService

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -17,10 +17,10 @@ export class MailService {
   constructor(private config: ConfigService) {
     this.transporter = nodemailer.createTransport(
       {
-        // host: String(process.env.MAIL_HOST),
-        // port: Number(process.env.MAIL_PORT),
-        // secure: Boolean(process.env.MAIL_SECURE),
-        service: "gmail",
+        host: String(process.env.MAIL_HOST),
+        port: Number(process.env.MAIL_PORT),
+        secure: Boolean(process.env.MAIL_SECURE),
+        // service: "gmail",
         auth: {
           user: String(process.env.MAIL_AUTH_USER),
           pass: String(process.env.MAIL_AUTH_PASS),
